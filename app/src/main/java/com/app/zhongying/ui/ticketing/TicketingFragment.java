79ac88732366b8ed1fd3a752b9d9c2178112036c
tianbao722 <tianbao20010722>
2020-09-08 15:19:30 +0800

    123

--- app/src/main/java/com/app/zhongying/ui/ticketing/TicketingFragment.java
+++ app/src/main/java/com/app/zhongying/ui/ticketing/TicketingFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.app.zhongying.R;
+
 public class TicketingFragment extends Fragment {
 
     public TicketingFragment() {
@@ -20,7 +21,8 @@ public class TicketingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ticketing, container, false);
+        View inflate = inflater.inflate(R.layout.fragment_ticketing, container, false);
+        return inflate;
 
     }
 }
