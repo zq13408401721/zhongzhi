37129e7ddef54a95905c77284b5055a5b025c44c
tianbao722 <tianbao20010722>
2020-09-08 15:17:15 +0800

    三组

--- app/src/main/java/com/app/zhongying/ui/ticketing/TicketingFragment.java
+++ app/src/main/java/com/app/zhongying/ui/ticketing/TicketingFragment.java
@@ -21,5 +21,6 @@ public class TicketingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ticketing, container, false);
+
     }
 }
