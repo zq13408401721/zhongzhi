29fdf1bb73ecfd20b6a26812c3e972c4b4297f2b
Angel-lhq <1165850812@qq.com>
2020-09-08 14:28:33 +0800

    lhq

--- app/src/main/java/com/app/zhongying/ui/live/LiveFragment.java
+++ app/src/main/java/com/app/zhongying/ui/live/LiveFragment.java
@@ -1,26 +1,23 @@
-package com.app.zhongying.ui.home;
+package com.app.zhongying.ui.live;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.app.zhongying.R;
 
-public class HomeFragment extends Fragment {
+public class LiveFragment extends Fragment {
 
-   public HomeFragment(){}
+    public LiveFragment(){}
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        return inflater.inflate(R.layout.fragment_live, container, false);
     }
 }
---
+++ app/src/main/java/com/app/zhongying/ui/own/OwnFragment.java
@@ -0,0 +1,22 @@
+package com.app.zhongying.ui.own;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.app.zhongying.R;
+
+public class OwnFragment extends Fragment {
+
+   public OwnFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_own, container, false);
+    }
+}
