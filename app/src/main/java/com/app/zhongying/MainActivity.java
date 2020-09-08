752ad3ef7c286ccf49aa7f3539204230cd5e7d92
xue03 <31613535005@qq.com>
2020-09-08 15:23:02 +0800

    xuett

--- app/src/main/java/com/app/zhongying/MainActivity.java
+++ app/src/main/java/com/app/zhongying/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
         //kkjjjkjghdfhgdhfgsfgfj
+        //kkjjjkjghdfhgdhfgsfgfj
     }
 
 }
