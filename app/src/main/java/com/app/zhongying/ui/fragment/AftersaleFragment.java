3ebbf53dc50e51f2d5d9d682f2c3b4cda92492af
lijingbao123 <344497381@qq.com>
2020-09-09 10:36:12 +0800

    李靖保

--- app/src/main/java/com/app/zhongying/ui/fragment/AftersaleFragment.java
+++ app/src/main/java/com/app/zhongying/ui/fragment/AftersaleFragment.java
@@ -1,4 +1,4 @@
-package com.app.zhongying.ui.live;
+package com.app.zhongying.ui.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,25 +14,22 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.app.zhongying.R;
 import com.app.zhongying.ui.adapter.AftersaleAdapter;
 
-public class LiveFragment extends Fragment {
-
-   /* private RecyclerView fragment_aftersale_rv2;*/
-
-    public LiveFragment() {
-    }
+public class AftersaleFragment extends Fragment {
+    private RecyclerView fragment_aftersale_rv;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_live, container, false);
+        View view = inflater.inflate(R.layout.fragment_aftersale, container, false);
         initView(view);
         return view;
+
     }
 
     private void initView(View view) {
-        /*fragment_aftersale_rv2 = view.findViewById(R.id.fragment_aftersale_rv2);
-        fragment_aftersale_rv2.setLayoutManager(new LinearLayoutManager(getActivity()));
+        fragment_aftersale_rv = view.findViewById(R.id.fragment_aftersale_rv);
+        fragment_aftersale_rv.setLayoutManager(new LinearLayoutManager(getActivity()));
         AftersaleAdapter aftersaleAdapter = new AftersaleAdapter(getActivity());
-        fragment_aftersale_rv2.setAdapter(aftersaleAdapter);*/
+        fragment_aftersale_rv.setAdapter(aftersaleAdapter);
     }
 }
--- app/src/main/java/com/app/zhongying/ui/fragment/EvaluateFragment.java
+++ app/src/main/java/com/app/zhongying/ui/fragment/EvaluateFragment.java
@@ -1,5 +1,6 @@
-package com.app.zhongying.ui.own;
+package com.app.zhongying.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,32 +18,31 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.app.zhongying.R;
 import com.app.zhongying.ui.adapter.EvaluateAdapter;
 
-public class OwnFragment extends Fragment {
-
-  /*  private TextView evaluate_Individual_stores;
+public class EvaluateFragment extends Fragment {
+//tv
+    private TextView evaluate_Individual_stores;
     private TextView evaluate_zuzu_time_tv;
     private ImageView evaluate_zuzu_img;
     private TextView evaluate_zuzu_contont_tv;
     private TextView evaluate_zuzu_single_tv;
     private TextView evaluate_zuzu_price_tv;
-    private Button button;
     private TextView evaluate_zuzu_like_tv;
-    private RecyclerView evaluate_zuzu_rv;*/
+    //but
+    private Button button;
 
-    public OwnFragment() {
-    }
+   //rv
+    private RecyclerView evaluate_zuzu_rv;
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_own, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_evaluate, container, false);
         initView(view);
         return view;
     }
 
     private void initView(View view) {
-      /*  evaluate_Individual_stores = view.findViewById(R.id.evaluate_Individual_stores);
+        evaluate_Individual_stores = view.findViewById(R.id.evaluate_Individual_stores);
         evaluate_zuzu_time_tv = view.findViewById(R.id.evaluate_zuzu_time_tv);
         evaluate_zuzu_img = view.findViewById(R.id.evaluate_zuzu_img);
         evaluate_zuzu_contont_tv = view.findViewById(R.id.evaluate_zuzu_contont_tv);
@@ -51,9 +53,16 @@ public class OwnFragment extends Fragment {
         evaluate_zuzu_rv = view.findViewById(R.id.evaluate_zuzu_rv);
 
         evaluate_zuzu_rv.setLayoutManager(new GridLayoutManager(getActivity(),2));
+
         EvaluateAdapter evaluateAdapter = new EvaluateAdapter(getActivity());
-        evaluate_zuzu_rv.setAdapter(evaluateAdapter);*/
+        evaluate_zuzu_rv.setAdapter(evaluateAdapter);
 
-
+       button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), EvaluateActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
