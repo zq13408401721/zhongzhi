065740b408c5948a698fb109b799b8aa43877265
longxinpeng901X <lxp860141483@qq.com>
2020-09-09 09:12:09 +0800

    longxinfanswitch

--- app/src/main/java/com/app/zhongying/MainActivity.java
+++ app/src/main/java/com/app/zhongying/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
                 switch (){
-
+                    ///hahahhahahhahahhahahha
                 }
             }
         });
