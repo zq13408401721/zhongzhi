ad9db1e55eeb90affe272201a7c5ab248ef35bd6
longxinpeng901X <lxp860141483@qq.com>
2020-09-09 14:32:11 +0800

    longxinfan合并YKL

--- app/src/main/java/com/app/zhongying/activity/own/Modify_DateActivity.java
+++ app/src/main/java/com/app/zhongying/activity/own/Modify_DateActivity.java
@@ -1,5 +1,15 @@
 package com.app.zhongying.activity.own;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,6 +17,19 @@ import android.os.Bundle;
 import com.app.zhongying.R;
 
 public class Modify_DateActivity extends AppCompatActivity {
+
+    private ImageView date_returns;
+    private TextView date_title;
+    private TextView date_preservation;
+    private EditText year1;
+    private EditText year2;
+    private EditText year3;
+    private EditText year4;
+    private EditText month1;
+    private EditText month2;
+    private EditText day;
+    private EditText day1;
+    private LinearLayout layou;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/app/zhongying/activity/own/ServiceActivity.java
+++ app/src/main/java/com/app/zhongying/activity/own/ServiceActivity.java
@@ -3,14 +3,37 @@ package com.app.zhongying.activity.own;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 import com.app.zhongying.R;
 
 public class ServiceActivity extends AppCompatActivity {
 
+    private ImageView returns;
+    private TextView title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        initView();
+    }
+
+    private void initView() {
+        returns = (ImageView) findViewById(R.id.returns);
+        title = (TextView) findViewById(R.id.title);
+        returns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
