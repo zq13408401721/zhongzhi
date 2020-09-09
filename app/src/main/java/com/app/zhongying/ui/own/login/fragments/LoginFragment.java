b9c755a663a2621a3eb08e62d1f3118a8632a85a
Captionj <IronmanJw@163.com>
2020-09-09 21:47:49 +0800

    登录注册功能完全版

--- app/src/main/java/com/app/zhongying/ui/own/login/fragments/LoginFragment.java
+++ app/src/main/java/com/app/zhongying/ui/own/login/fragments/LoginFragment.java
@@ -2,12 +2,18 @@ package com.app.zhongying.ui.own.login.fragments;
 
 import android.app.Application;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -27,13 +33,10 @@ public class LoginFragment extends Fragment {
     private ImageView mVisi;
     private Button mLong_btn;
     private Button mLong_wechat_not;
-    private Button mLong_wechat;
-    private ImageView mSelect_yes;
-    private ImageView mSelect_no;
+    private CheckBox mSelect_no;
     private TextView mForget;
-    private String mPhone_num;
-    private String mPhone_pwd;
     private boolean flag = true;
+
 
     @Nullable
     @Override
@@ -50,58 +53,73 @@ public class LoginFragment extends Fragment {
         mVisi = view.findViewById(R.id.login_visi);
         mLong_btn = view.findViewById(R.id.long_btn);
         mLong_wechat_not = view.findViewById(R.id.long_wechat_not);
-        mLong_wechat = view.findViewById(R.id.long_wechat);
-        mSelect_yes = view.findViewById(R.id.select_yes);
         mSelect_no = view.findViewById(R.id.select_no);
         mForget = view.findViewById(R.id.login_forget);
         initData();
     }
 
     private void initData() {
-       mForget.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               Intent intent = new Intent(getActivity(), ForgetActivity.class);
-               startActivity(intent);
-           }
-       });
-        mPhone_num = mPhone.getText().toString().trim();
-        mPhone_pwd = mPwd.getText().toString().trim();
+        mForget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ForgetActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        /*//  去除中文
+        mPwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.length()>0){
+                    for (int i = 0; i < editable.length(); i++) {
+                        char c = editable.charAt(i);
+                        if (c >= 0x4e00 && c <= 0X9fff) { // 根据字节码判断
+                            // 如果是中文，则清除输入的字符，否则保留
+                            editable.delete(i,i+1);
+                        }
+                    }
+                }
+                String pwd = mPwd.getText().toString();
+                    pwd = editable.toString();
+            }
+        });
+*/
         mLong_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(StringUtils.inputPhone(mPhone_num)){
-                    if (StringUtils.inputEnglish(mPhone_pwd)){
-
-                    } else if (StringUtils.inputNum(mPhone_pwd)){
-                        Toast.makeText(getContext(),"注册成功",Toast.LENGTH_SHORT).show();
-
-                    }else if (StringUtils.inputJudge(mPhone_pwd)){
-                        Toast.makeText(getContext(),"注册成功",Toast.LENGTH_SHORT).show();
-
-                    }else {
-                        Toast.makeText(getContext(),"密码不符合标准",Toast.LENGTH_SHORT).show();
-                    }
-                }else {
-                    Toast.makeText(getContext(),"手机号不符合标准",Toast.LENGTH_SHORT).show();
-
+                String Phone_num = mPhone.getText().toString();
+                String Pwd = mPwd.getText().toString();
+                if (!StringUtils.isMobile(Phone_num)) {
+                    Toast.makeText(getActivity(), "电话号不符合标准", Toast.LENGTH_SHORT).show();
+                } else if (Pwd.isEmpty()) {
+                    Toast.makeText(getActivity(), "密码不能为空", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), "登录成功", Toast.LENGTH_SHORT).show();
                 }
             }
         });
         mSelect_no.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mSelect_no.getVisibility() == View.VISIBLE){
-                    mSelect_no.setVisibility(View.INVISIBLE);
-                    mSelect_yes.setVisibility(View.VISIBLE);
-                    mLong_wechat_not.setVisibility(View.INVISIBLE);
-                    mLong_wechat.setVisibility(View.VISIBLE);
-                }else {
-                    mSelect_no.setVisibility(View.VISIBLE);
-                    mSelect_yes.setVisibility(View.INVISIBLE);
-                    mLong_wechat_not.setVisibility(View.VISIBLE);
-                    mLong_wechat.setVisibility(View.INVISIBLE);
+                if (mSelect_no.isSelected()) {
+                    mSelect_no.setChecked(false);
+                    mSelect_no.setSelected(false);
+                    mLong_wechat_not.setBackgroundDrawable(getResources().getDrawable(R.drawable.gray_shap));
+                } else if (!mSelect_no.isSelected()) {
+                    mSelect_no.setChecked(true);
+                    mSelect_no.setSelected(true);
+                    mLong_wechat_not.setBackgroundDrawable(getResources().getDrawable(R.drawable.green_shap));
                 }
             }
         });
@@ -110,12 +128,10 @@ public class LoginFragment extends Fragment {
 
             @Override
             public void onClick(View view) {
-                if (flag) {
-                    mPwd.setInputType(View.AUTOFILL_TYPE_NONE);
-                    flag=false;
-                }else{
-                    mPwd.setInputType(mPwd.getInputType());
-                    flag=true;
+                if (mVisi.isSelected()) {
+                    mPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                } else {
+                    mPwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
                 }
             }
         });
--- app/src/main/java/com/app/zhongying/ui/own/login/fragments/RegisterFragment.java
+++ app/src/main/java/com/app/zhongying/ui/own/login/fragments/RegisterFragment.java
@@ -2,10 +2,13 @@ package com.app.zhongying.ui.own.login.fragments;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,6 +26,14 @@ public class RegisterFragment extends Fragment {
 
 
     private Button mRegister_ver;
+    private CheckBox mselect_register;
+    private Button mbtn_register;
+    private EditText mRegister_phone;
+    private EditText mVerification;
+    private EditText mRegister_verCode;
+    private EditText mRegister_pwd;
+    private boolean isOK = false;
+    private ImageView mRegister_visi;
 
     @Nullable
     @Override
@@ -35,6 +46,13 @@ public class RegisterFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mRegister_ver = view.findViewById(R.id.register_ver);
+        mselect_register = view.findViewById(R.id.select_register);
+        mbtn_register = view.findViewById(R.id.btn_register);
+        mRegister_phone = view.findViewById(R.id.register_phone);
+        mVerification = view.findViewById(R.id.verification);
+        mRegister_verCode = view.findViewById(R.id.register_verCode);
+        mRegister_pwd = view.findViewById(R.id.register_pwd);
+        mRegister_visi = view.findViewById(R.id.register_visi);
         mRegister_ver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,7 +60,7 @@ public class RegisterFragment extends Fragment {
                 CountDownTimer timer = new CountDownTimer(60000, 1000) {
                     @Override
                     public void onTick(long l) {
-                        mRegister_ver.setText("获取验证码（"+l/1000+")秒");
+                        mRegister_ver.setText("获取验证码（" + l / 1000 + ")秒");
                     }
 
                     @Override
@@ -51,6 +69,43 @@ public class RegisterFragment extends Fragment {
                     }
                 };
                 timer.start();
+            }
+        });
+        mselect_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mselect_register.isSelected()) {
+                    mselect_register.setChecked(false);
+                    mselect_register.setSelected(false);
+                } else if (!mselect_register.isSelected()) {
+                    mselect_register.setChecked(true);
+                    mselect_register.setSelected(true);
+                }
+            }
+        });
+        mbtn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!StringUtils.isMobile(mRegister_phone.getText().toString())) {
+                    Toast.makeText(getActivity(), "手机号不符合标准", Toast.LENGTH_SHORT).show();
+                } else if (mVerification.getText().toString().isEmpty()) {
+                    Toast.makeText(getActivity(), "验证码不能为空", Toast.LENGTH_SHORT).show();
+                } else if (mRegister_pwd.getText().toString().isEmpty()) {
+                    Toast.makeText(getActivity(), "密码不能为空", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), "注册成功", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        mRegister_visi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mRegister_visi.isSelected()) {
+                    mRegister_pwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                } else {
+                    mRegister_pwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
             }
         });
     }
