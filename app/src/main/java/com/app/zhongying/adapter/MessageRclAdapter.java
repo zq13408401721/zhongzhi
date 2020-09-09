8b550b83548561e57241cbe3d615b746590c3f07
Angel-lhq <1165850812@qq.com>
2020-09-09 16:52:23 +0800

    消息中心

--- app/src/main/java/com/app/zhongying/adapter/MessageRclAdapter.java
+++ app/src/main/java/com/app/zhongying/adapter/MessageRclAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,7 +32,7 @@ public class MessageRclAdapter extends RecyclerView.Adapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View inflate = LayoutInflater.from(context).inflate(R.layout.adapter_message_item, parent, false);
-        return null;
+        return new HolderItem(inflate);
     }
 
     @Override
@@ -40,6 +42,20 @@ public class MessageRclAdapter extends RecyclerView.Adapter {
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 6;
+    }
+
+    private class HolderItem extends RecyclerView.ViewHolder {
+
+        private ImageView imgHead;
+        private TextView tvName;
+        private TextView tvContent;
+
+        public HolderItem(View inflate) {
+            super(inflate);
+            imgHead = inflate.findViewById(R.id.img_head);
+            tvName = inflate.findViewById(R.id.tv_name);
+            tvContent = inflate.findViewById(R.id.tv_content);
+        }
     }
 }
--- app/src/main/java/com/app/zhongying/ui/own/fragments/messagefragments/InteractionFragment.java
+++ app/src/main/java/com/app/zhongying/ui/own/fragments/messagefragments/InteractionFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.app.zhongying.R;
+import com.app.zhongying.adapter.MessageRclAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -50,6 +52,10 @@ public class InteractionFragment extends Fragment {
 
     private void initView() {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        MessageRclAdapter messageRclAdapter = new MessageRclAdapter(getContext());
+        recyclerView.setAdapter(messageRclAdapter);
+        //添加Android自带的分割线
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL));
     }
 
     @Override
