5186b7636a4b61dc8e2c2606141b96317c70150b
air <1257078953@qq.com>
2020-09-09 09:51:44 +0800

    pdx

--- app/src/main/java/com/app/zhongying/ui/home/HomeFragment.java
+++ app/src/main/java/com/app/zhongying/ui/home/HomeFragment.java
@@ -1,26 +1,44 @@
 package com.app.zhongying.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.app.zhongying.R;
+import com.app.zhongying.SeekActivity;
+
+import butterknife.BindView;
 
 public class HomeFragment extends Fragment {
 
-   public HomeFragment(){}
+
+    public HomeFragment() {
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initView(view);
+        return view;
+
+    }
+
+    private void initView(View view) {
+        LinearLayout layout = view.findViewById(R.id.layout);
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), SeekActivity.class));
+            }
+        });
     }
 }
