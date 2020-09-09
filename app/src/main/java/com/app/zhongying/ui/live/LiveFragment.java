eb3984bab4aa7c5454bb109e2cacf76d7b493f5f
pzb001025 <1275776996@qq.com>
2020-09-09 10:11:52 +0800

    个人中心布局

--- app/src/main/java/com/app/zhongying/ui/live/LiveFragment.java
+++ app/src/main/java/com/app/zhongying/ui/live/LiveFragment.java
@@ -18,6 +18,5 @@ public class LiveFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_live, container, false);
-
     }
 }
