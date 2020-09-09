bedfcf7d0aaab58809aa5f9e44429677f875023b
Angel-lhq <1165850812@qq.com>
2020-09-09 14:13:34 +0800

    导航切换更改标题

--- app/src/main/java/com/app/zhongying/MainActivity.java
+++ app/src/main/java/com/app/zhongying/MainActivity.java
@@ -2,6 +2,8 @@ package com.app.zhongying;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,6 +13,9 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +32,31 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        TextView tvTitle = findViewById(R.id.tv_title);
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                navController.navigate(item.getItemId());
+                switch (item.getItemId()){
+                    case R.id.navigation_home:
+                        tvTitle.setText(getResources().getString(R.string.title_home));
+                        break;
+                    case R.id.navigation_shop:
+                        tvTitle.setText(getResources().getString(R.string.title_shop));
+                        break;
+                    case R.id.navigation_ticketing:
+                        tvTitle.setText(getResources().getString(R.string.title_ticketing));
+                        break;
+                    case R.id.navigation_live:
+                        tvTitle.setText(getResources().getString(R.string.title_live));
+                        break;
+                    case R.id.navigation_own:
+                        tvTitle.setText(getResources().getString(R.string.title_own));
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
 }
