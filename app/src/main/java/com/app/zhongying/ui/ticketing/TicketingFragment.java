2bdfce7830c43b5f5d3aa805451d7081819cb920
zheng_xiong_xy <2330192169@qq.com>
2020-09-09 14:09:47 +0800

    订票页面

--- app/src/main/java/com/app/zhongying/ui/ticketing/TicketingFragment.java
+++ app/src/main/java/com/app/zhongying/ui/ticketing/TicketingFragment.java
@@ -1,5 +1,6 @@
 package com.app.zhongying.ui.ticketing;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.app.zhongying.R;
 public class TicketingFragment extends Fragment {
@@ -20,6 +22,14 @@ public class TicketingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ticketing, container, false);
+        View view = inflater.inflate(R.layout.fragment_ticketing, container, false);
+        Button btn = view.findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(),ReserveActivity.class));
+            }
+        });
+        return view;
     }
 }
