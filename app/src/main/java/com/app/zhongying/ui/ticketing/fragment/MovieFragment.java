7d9464db0d37e33024dda34f593487328dfcff71
tianbao722 <tianbao20010722>
2020-09-09 09:45:18 +0800

    票务

---
+++ app/src/main/java/com/app/zhongying/ui/ticketing/fragment/MovieFragment.java
@@ -0,0 +1,24 @@
+package com.app.zhongying.ui.ticketing.fragment;
+
+import android.os.Bundle;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.app.zhongying.R;
+
+public class MovieFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View inflate = inflater.inflate(R.layout.fragment_movie, container, false);
+        return inflate;
+    }
+
+
+}
