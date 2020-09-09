221914fb9e277c1b8abe2889729f0493750d5949
一直在 <7808727+it_has_been_staying_here@user.noreply.gitee.com>
2020-09-09 19:30:55 +0800

    ykl

--- app/src/main/java/com/app/zhongying/activity/own/MarketingActivity.java
+++ app/src/main/java/com/app/zhongying/activity/own/MarketingActivity.java
@@ -3,31 +3,35 @@ package com.app.zhongying.activity.own;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.zhongying.R;
 
-public class Modify_DateActivity extends AppCompatActivity implements View.OnClickListener {
+public class MarketingActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageView date_returns;
+    private ImageView returns;
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_modify__date);
+        setContentView(R.layout.activity_marketing);
         initView();
     }
 
     private void initView() {
-        date_returns = (ImageView) findViewById(R.id.date_returns);
-        date_returns.setOnClickListener(this);
+        returns = (ImageView) findViewById(R.id.returns);
+        returns.setOnClickListener(this);
+        title = (TextView) findViewById(R.id.title);
+        title.setText("营销利润");
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.date_returns:
+            case  R.id.returns:
                 finish();
                 break;
         }
