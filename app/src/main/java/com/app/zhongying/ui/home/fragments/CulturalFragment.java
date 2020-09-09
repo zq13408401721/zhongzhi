72f8d539d036478643582124648a33ec5e718e42
liukai0305 <1603952180@qq.com>
2020-09-09 14:24:04 +0800

    刘凯

--- app/src/main/java/com/app/zhongying/ui/home/fragments/CulturalFragment.java
+++ app/src/main/java/com/app/zhongying/ui/home/fragments/CulturalFragment.java
@@ -1,66 +1,42 @@
 package com.app.zhongying.ui.home.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.app.zhongying.R;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CulturalFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.app.zhongying.R;
+import com.app.zhongying.adapter.RecommendAdapter;
+
+import java.util.ArrayList;
+
+
 public class CulturalFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CulturalFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CulturalFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CulturalFragment newInstance(String param1, String param2) {
-        CulturalFragment fragment = new CulturalFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private RecyclerView mRcy;
+    private RecommendAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cultural, container, false);
+        View inflate = inflater.inflate(R.layout.fragment_cultural, container, false);
+        initView(inflate);
+        return inflate;
+
+    }
+
+    private void initView(View inflate) {
+        mRcy = inflate.findViewById(R.id.rcy);
+        mRcy = inflate.findViewById(R.id.rcy);
+        mRcy.setLayoutManager(new GridLayoutManager(getContext(),2));
+        ArrayList<String> list = new ArrayList<>();
+        adapter = new RecommendAdapter(list, getActivity());
+        mRcy.setAdapter(adapter);
     }
 }
