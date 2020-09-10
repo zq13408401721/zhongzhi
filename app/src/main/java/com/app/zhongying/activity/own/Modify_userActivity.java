485b1b6ec98fa5917948b8c1168c7c53e09ae98b
一直在 <7808727+it_has_been_staying_here@user.noreply.gitee.com>
2020-09-10 11:27:42 +0800

    ykl

--- app/src/main/java/com/app/zhongying/activity/own/Modify_userActivity.java
+++ app/src/main/java/com/app/zhongying/activity/own/Modify_userActivity.java
@@ -1,7 +1,12 @@
 package com.app.zhongying.activity.own;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -10,13 +15,19 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.aigestudio.wheelpicker.WheelPicker;
 import com.app.zhongying.R;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,17 +36,17 @@ public class Modify_userActivity extends AppCompatActivity implements View.OnCli
     private ImageView returns;
     private TextView title;
     private ImageView modify_head_img;
-    private ImageView modify_head;
+    private RelativeLayout modify_head;
     private TextView modify_name;
-    private ImageView modify_name_img;
+    private RelativeLayout modify_name_img;
     private TextView modify_resume;
-    private ImageView modify_resume_img;
+    private RelativeLayout modify_resume_img;
     private TextView modify_sex;
-    private ImageView modify_sex_img;
+    private RelativeLayout modify_sex_img;
     private TextView modify_age;
-    private ImageView modify_age_img;
+    private RelativeLayout modify_age_img;
     private TextView modify_phone;
-    private ImageView modify_phone_img;
+    private RelativeLayout modify_phone_img;
     private TextView photograph;
     private TextView album;
     private TextView cancel;
@@ -45,6 +56,8 @@ public class Modify_userActivity extends AppCompatActivity implements View.OnCli
     private WheelPicker sex;
     private TextView cancel_sex;
     private TextView determine;
+    private View contentView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,39 +73,31 @@ public class Modify_userActivity extends AppCompatActivity implements View.OnCli
         layout = (LinearLayout) findViewById(R.id.line);
         modify_head_img = (ImageView) findViewById(R.id.modify_head_img);
 
-        modify_head = (ImageView) findViewById(R.id.modify_head);
+        modify_head = (RelativeLayout) findViewById(R.id.modify_head);
         modify_head.setOnClickListener(this);
         modify_name = (TextView) findViewById(R.id.modify_name);
-        modify_name_img = (ImageView) findViewById(R.id.modify_name_img);
+        modify_name_img = (RelativeLayout) findViewById(R.id.modify_name_img);
         modify_name_img.setOnClickListener(this);
         modify_resume = (TextView) findViewById(R.id.modify_resume);
-        modify_resume_img = (ImageView) findViewById(R.id.modify_resume_img);
+        modify_resume_img = (RelativeLayout) findViewById(R.id.modify_resume_img);
         modify_resume_img.setOnClickListener(this);
         modify_sex = (TextView) findViewById(R.id.modify_sex);
-        modify_sex_img = (ImageView) findViewById(R.id.modify_sex_img);
+        modify_sex_img = (RelativeLayout) findViewById(R.id.modify_sex_img);
         modify_sex_img.setOnClickListener(this);
         modify_age = (TextView) findViewById(R.id.modify_age);
-        modify_age_img = (ImageView) findViewById(R.id.modify_age_img);
+        modify_age_img = (RelativeLayout) findViewById(R.id.modify_age_img);
         modify_age_img.setOnClickListener(this);
         modify_phone = (TextView) findViewById(R.id.modify_phone);
-        modify_phone_img = (ImageView) findViewById(R.id.modify_phone_img);
+        modify_phone_img = (RelativeLayout) findViewById(R.id.modify_phone_img);
         modify_phone_img.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.modify_head:
-                View contentView = LayoutInflater.from(this).inflate(R.layout.head_popupwindow, null);
-                photograph = contentView.findViewById(R.id.photograph);
-                album = contentView.findViewById(R.id.album);
-                cancel = contentView.findViewById(R.id.cancel);
-                cancel.setOnClickListener(this);
-                popWnd = new PopupWindow(contentView,ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
-
-               popWnd.showAtLocation(layout, Gravity.CENTER_VERTICAL,0,0);
-
-
+                onphotograph();
                 break;
             case R.id.modify_name_img:
                 Intent nameintent = new Intent(this,Modify_nameActivity.class);
@@ -131,9 +136,7 @@ public class Modify_userActivity extends AppCompatActivity implements View.OnCli
                 break;
             case R.id.modify_phone_img:
                 break;
-            case R.id.cancel:
-                popWnd.dismiss();
-                break;
+
             case R.id.cancel_sex:
                 popWnds.dismiss();
                 break;
@@ -143,8 +146,120 @@ public class Modify_userActivity extends AppCompatActivity implements View.OnCli
             case R.id.returns:
                 finish();
                 break;
+
         }
     }
+
+    private void onphotograph() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            final AlertDialog dialog = builder.create();
+        contentView = LayoutInflater.from(this).inflate(R.layout.head_popupwindow, null);
+        photograph = contentView.findViewById(R.id.photograph);
+        popWnd = new PopupWindow(contentView,ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+
+        popWnd.showAtLocation(layout, Gravity.CENTER_VERTICAL,0,0);
+        album = contentView.findViewById(R.id.album);
+        cancel = contentView.findViewById(R.id.cancel);
+        album.setOnClickListener(new View.OnClickListener() {// 在相册中选取
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(Intent.ACTION_PICK, null);
+                intent1.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent1, 1);
+                dialog.dismiss();
+            }
+        });
+        photograph.setOnClickListener(new View.OnClickListener() {// 调用照相机
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent2.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(new File(Environment.getExternalStorageDirectory(), "head.jpg")));
+                startActivityForResult(intent2, 2);// 采用ForResult打开
+                dialog.dismiss();
+            }
+        });
+        dialog.setView(contentView);
+      //  dialog.show();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    cropPhoto(data.getData());// 裁剪图片
+                }
+                break;
+            case 2:
+                if (resultCode == RESULT_OK) {
+                    File temp = new File(Environment.getExternalStorageDirectory() + "/head.jpg");
+                    cropPhoto(Uri.fromFile(temp));// 裁剪图片
+                }
+                break;
+            case 3:
+                if (data != null) {
+                    Bundle extras = data.getExtras();
+                   Bitmap head = extras.getParcelable("data");
+                    if (head != null) {
+/**
+ * 上传服务器代码
+ */
+                        setPicToView(head);// 保存在SD卡中
+                        modify_head_img.setImageBitmap(head);// 用ImageView显示出来
+                        popWnd.dismiss();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    /**
+     * 调用系统的裁剪功能
+     *
+     * @param uri
+     */
+    public void cropPhoto(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", "true");
+// aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+// outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, 3);
+    }
+    private void setPicToView(Bitmap mBitmap) {
+        String sdStatus = Environment.getExternalStorageState();
+        if (!sdStatus.equals(Environment.MEDIA_MOUNTED)) { // 检测sd是否可用
+            return;
+        }
+        FileOutputStream b = null;
+         String path = "/sdcard/myHead/";// sd路径
+        File file = new File(path);
+        file.mkdirs();// 创建文件夹
+        String fileName = path + "head.jpg";// 图片名字
+        try {
+            b = new FileOutputStream(fileName);
+            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, b);// 把数据写入文件
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+// 关闭流
+                b.flush();
+                b.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 
     @Override
     public void onItemSelected(WheelPicker picker, Object data, int position) {
